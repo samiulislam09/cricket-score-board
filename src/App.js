ad5af2dc5b7c486f866d36f99e814a404277e714
samiulislam09 <samiulislamsawon09@gmail.com>
2022-10-22 21:52:24 +0600

    added buttons

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [run, setRun] = useState(0)
+  const [wicket, setWicket] = useState(0)
+  const [noBalls, setNoBalls] = useState(0)
+  const [wideBalls, setWideBalls] = useState(0)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className='container'>
+        <div className="batting">
+          <button onClick={() => { setRun(run+1) }}>1 run</button>
+          <button onClick={() => { setRun(run +2) }}>2 run</button>
+          <button onClick={() => { setRun(run +3) }}>3 run</button>
+          <button onClick={() => { setRun(run+4) }}>4 run</button>
+          <button onClick={() => { setRun(run+6) }}>6 run</button>
+          <p>Batting Team</p>
+          <p>{run}/{ wicket }</p>
+        </div>
+        <div className="fielding">
+          <button onClick={() => { setWicket(wicket + 1) }}>Wicket</button>
+          <button onClick={() => {
+            setNoBalls(noBalls + 1);
+            setRun(run+1)
+          }}>No Balls</button>
+          <button onClick={() => {
+            setWideBalls(wideBalls + 1);
+            setRun(run + 1)
+          }}>Wide Balls</button>
+          <p>{wicket} Wickets</p>
+          <p>{noBalls} No balls</p>
+          <p>{wideBalls} wide balls</p>
+        </div>
+
+      </section>
     </div>
   );
 }
